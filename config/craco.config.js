54f348f403674964221086848640b0206e78ddd0
Safin Maksim <a4ron5@yandex.ru>
2020-01-11 14:47:25 +0500

    init

---
+++ config/craco.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const CracoAlias = require("craco-alias")
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "jsconfig",
+      },
+    },
+  ],
+}
